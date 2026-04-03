dc872790269ce5069cb028d45ebe8183636f0819
Davi Santos <davisimoesdossantos1@gmail.com>
2026-04-03 11:12:12 -0300

    fix: solved iter template issue on const array

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: dasimoes <dasimoes@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 12:14:49 by dasimoes          #+#    #+#             */
-/*   Updated: 2026/03/30 19:15:03 by dasimoes         ###   ########.fr       */
+/*   Updated: 2026/04/03 11:08:59 by dasimoes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,21 @@ void	printConstElem(const T& elem)
 	std::cout << elem << " ";
 }
 
-template <typename T, typename F>
-void	iter(T* arr, const std::size_t size, F func)
+template <typename T>
+void	iter(const T* arr, const std::size_t size, void (*func)(T const &))
 {
+	if (!arr | !func)
+		return ;
 	for (std::size_t i = 0; i < size; i++)
 		func(arr[i]);
 }
 
+template <typename T>
+void	iter(T* arr, const std::size_t size, void (*func)(T &))
+{
+	if (!arr | !func)
+		return ;
+	for (std::size_t i = 0; i < size; i++)
+		func(arr[i]);
+}
 #endif
